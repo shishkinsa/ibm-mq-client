720f379b7431af7bd5aa580dc81673e392c2e16a
Sergey Shishkin <shishkin.s.a@gmail.com>
2022-02-11 18:58:10 +0300

    init project

---
+++ ibm-mq/ibm-mq/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+using IBM.WMQ;
+
+namespace MQSample
+{
+    class Program 
+    {
+        public static void Main(string[] args)
+        {
+            // The type of connection to use, this can be:-
+            // MQC.TRANSPORT_MQSERIES_BINDINGS for a server connection.
+            // MQC.TRANSPORT_MQSERIES_CLIENT for a non-XA client connection
+            // MQC.TRANSPORT_MQSERIES_XACLIENT for an XA client connection
+            // MQC.TRANSPORT_MQSERIES_MANAGED for a managed client connection
+            const String connectionType = MQC.TRANSPORT_MQSERIES_CLIENT;
+            
+            
+        }
+    }
+}
+
